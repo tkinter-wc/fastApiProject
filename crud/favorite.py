19fef122b11a8bcd67b17443ec3a49319c9f896c
weichang99 <2560692239@qq.com>
2026-08-04 18:43:49 +0800

    添加收藏，移除收藏

--- crud/favorite.py
+++ crud/favorite.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, delete
 
 from models.favorite import Favorite
 
@@ -13,3 +13,28 @@ async def is_news_favorite(
 
     # 是否有收藏记录
     return result.scalar_one_or_none() is not None
+
+
+async def add_news_favorite(
+        db: AsyncSession,
+        user_id: int,
+        news_id: int
+):
+    favorite = Favorite(user_id=user_id, news_id=news_id)
+    db.add(favorite)
+    await db.commit()
+    await db.refresh(favorite)
+
+    return favorite
+
+
+async def remove_news_favorite(
+        db: AsyncSession,
+        user_id: int,
+        news_id: int
+):
+    query = delete(Favorite).where(Favorite.user_id == user_id, Favorite.news_id == news_id)
+    result = await db.execute(query)
+    await db.commit()
+
+    return result.rowcount > 0
--- routers/favorite.py
+++ routers/favorite.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
 from config.db_conf import get_database
 from models.users import User
-from schemas.favorite import FavoriteCheckResponse
+from schemas.favorite import FavoriteCheckResponse, FavoriteAddRequest
 from utils.auth import get_current_user
 from utils.response import success_response
 from crud import favorite
@@ -20,3 +21,28 @@ async def check_favorite(
     is_favorited = await favorite.is_news_favorite(db, user.id, news_id)
 
     return success_response(message="检查收藏状态成功", data=FavoriteCheckResponse(isFavorite=is_favorited))
+
+
+@router.post("/add")
+async def add_favorite(
+        data: FavoriteAddRequest,
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_database)
+):
+    result = await favorite.add_news_favorite(db, user.id, data.news_id)
+
+    return success_response(message="添加收藏成功", data=result)
+
+
+@router.delete("/remove")
+async def remove_favorite(
+        news_id: int = Query(..., alias="newsId"),
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_database)
+):
+    result = await favorite.remove_news_favorite(db, user.id, news_id)
+
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="收藏记录不存在")
+
+    return success_response(message="删除收藏成功", data=result)
